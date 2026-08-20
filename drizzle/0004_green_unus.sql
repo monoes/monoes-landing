2bfb89f8a12d10a6a1c84ad473452b5af29f72ee
morteza <5457777+nokhodian@users.noreply.github.com>
2026-08-20 16:20:03 +0200

    feat(db): add profile columns to user table

---
+++ drizzle/0004_green_unus.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `user` ADD `tagline` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `job_title` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `company` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `tags_json` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `github_url` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `twitter_url` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `linkedin_url` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `website_url` text;--> statement-breakpoint
+ALTER TABLE `user` ADD `avatar_key` text;
